87b42bcebaa4d11f14c28aea525f2fbb184f869e
Liis R <liisvoks@gmail.com>
2022-11-23 23:25:28 +0200

    RegisterView

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,58 @@
+<template>
+
+
+  <div class="container mt-5">
+    <div class="row justify-content-around">
+      <div class="col-4">
+        <div class="row input-group mb-4">
+          <span class="input-group-text" id="inputGroup-sizing-default">Eesnimi*</span>
+          <input type="text" class="form-control" aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-default">
+        </div>
+        <div class="row input-group mb-4">
+          <span class="input-group-text" id="inputGroup-sizing-default">Perekonnanimi*</span>
+          <input type="text" class="form-control" aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-default">
+        </div>
+        <div class="row input-group mb-4">
+          <span class="input-group-text" id="inputGroup-sizing-default">e-posti aadress*</span>
+          <input type="text" class="form-control" aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-default">
+        </div>
+        <div class="row input-group mb-4">
+          <span class="input-group-text" id="inputGroup-sizing-default">Salasõna*</span>
+          <input type="text" class="form-control" aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-default">
+        </div>
+        <div class="input-group mb-4">
+          <span class="input-group-text" id="inputGroup-sizing-default">Korda salasõna*</span>
+          <input type="text" class="form-control" aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-default">
+        </div>
+        <div class="row">*kohustuslikud väljad</div>
+
+      </div>
+      <div class="col-8">
+        <img src="https://media.4rgos.it/i/Argos/0920-m0014-m007-m050-asym-m008-m022-adultbikeguide-7603802?maxW=768&qlt=75&fmt.jpeg.interlaced=true" alt="jalgratas">
+      </div>
+      <div class="col-6"></div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-4"></div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-4"></div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "RegisterView"
+}
+</script>
+
+<style scoped>
+
+</style>
