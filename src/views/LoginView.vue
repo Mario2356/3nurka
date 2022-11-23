ecf4e181ed14db3614f1144b28bf1d71521bc486
Liis R <liisvoks@gmail.com>
2022-11-23 16:30:07 +0200

    Login leht,input väljad

---
+++ src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+    <div class="container">
+      <div class="container text-center">
+        <div class="row align-items-center">
+
+          <div class="col">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+              <input type="text" class="form-control" aria-label="Sizing example input"
+                     aria-describedby="inputGroup-sizing-default">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+              <input type="text" class="form-control" aria-label="Sizing example input"
+                     aria-describedby="inputGroup-sizing-default">
+            </div>
+          </div>
+          <div class="col">
+
+          </div>
+          <div class="col">
+
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
