94dbfdd73d71e2b5f9230cdf1c6fcb84194a845f
Margit Toots <m.toots@hotmail.com>
2022-11-24 12:19:41 +0200

    commit, app.vue vaade

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Logi sisse</router-link> |
-    </nav>
-    <router-view/>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-2">
+
+          <div class="m-lg-2" style="width: 260px;">
+            <img src="logo_3nurka.png" alt="logo_3nurka" style="width:60%">
+          </div>
+
+        </div>
+
+
+        <div class="col d-inline-block">
+          <div id="app">
+            <nav>
+              <router-link to="/">Home</router-link>
+              |
+              <router-link to="/about">About</router-link>
+              |
+              <router-link to="/login">Logi sisse</router-link>
+              |
+            </nav>
+          </div>
+
+          <div class="row"></div>
+
+            <router-view/>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,59 +1,55 @@
 <template>
   <div>
-    <div class="container">
 
-    <div class="m-lg-2" style="width: 260px;">
-      <img src="logo_3nurka.png" alt="logo_3nurka" style="width:60%">
-    </div>
+<!--    <div class="container">-->
 
-    <ul class="nav justify-content-end">
-    <nav class="nav-fill navbar-light">
-      <form class="container-fluid justify-content-end">
-        <button class="btn btn-outline-dark me-2" type="button">Esileht</button>
-        <button class="btn btn-outline-dark me-2" type="button">Logi sisse</button>
-        <button class="btn btn-outline-dark me-2" type="button">Kontakt</button>
-      </form>
-    </nav>
-    </ul>
+<!--      <div class="m-lg-2" style="width: 260px;">-->
+<!--        <img src="logo_3nurka.png" alt="logo_3nurka" style="width:60%">-->
+<!--      </div>-->
+
+<!--      <ul class="nav justify-content-end">-->
+<!--        <nav class="nav-fill navbar-light">-->
+<!--          <form class="container-fluid justify-content-end">-->
+<!--            <button class="btn btn-outline-dark me-2" type="button">Esileht</button>-->
+<!--            <button class="btn btn-outline-dark me-2" type="button">Logi sisse</button>-->
+<!--            <button class="btn btn-outline-dark me-2" type="button">Kontakt</button>-->
+<!--          </form>-->
+<!--        </nav>-->
+<!--      </ul>-->
 
 
+      <div class="container">
+        <div class="container text-center">
+          <div class="row align-items-center">
 
-    <div class="container">
-      <div class="container text-center">
-        <div class="row align-items-center">
-
-          <div class="col">
-            <div class="input-group mb-3">
+            <div class="col">
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+                <input type="text" class="form-control" aria-label="Sizing example input"
+                       aria-describedby="inputGroup-sizing-default">
+              </div>
               <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
               <input type="text" class="form-control" aria-label="Sizing example input"
                      aria-describedby="inputGroup-sizing-default">
             </div>
-            <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
-            <input type="text" class="form-control" aria-label="Sizing example input"
-                   aria-describedby="inputGroup-sizing-default">
-          </div>
-          <div class="col">
+            <div class="col">
 
-          </div>
-          <div class="col">
+            </div>
+            <div class="col">
 
-          </div>
-        </div>
-        <div>
-
-        </div>
-        <div class="col-md-8">
-          <div class="thumbnail">
-            <img class="img-responsive" src="pilt_1.jpg" alt="pilt_1" style="width:100%">
-            <div class="caption">
             </div>
           </div>
+
+          <div class="col-md-8">
+            <div class="thumbnail">
+              <img class="img-responsive" src="pilt_1.jpg" alt="pilt_1" style="width:100%">
+              <div class="caption">
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
-
-      </div>
-    </div>
-
-
 
 
     </div>
