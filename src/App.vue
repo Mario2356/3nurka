2dc6e2068bd4222c356bed73ac4110a6441e2e02
Liis R <liisvoks@gmail.com>
2022-11-24 12:15:39 +0200

    RegisterView

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Logi sisse</router-link> |
-      <router-link to="/register">Loo konto</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <div class="container upper">
+      <div class="row">
+        <div class="col-2">
+          <img
+              src="https://media.4rgos.it/i/Argos/0920-m0014-m007-m050-asym-m008-m022-adultbikeguide-7603802?maxW=768&qlt=75&fmt.jpeg.interlaced=true"
+              style="width:60%"
+              alt="jalgratas">
+        </div>
+        <div class="col-6">
+          <div id="app">
+            <nav>
+              <router-link to="/">Home</router-link>
+              |
+              <router-link to="/about">About</router-link>
+              |
+              <router-link to="/login">Logi sisse</router-link>
+              |
+              <router-link to="/register">Loo konto</router-link>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+      </div>
+      <div class="row">
+      </div>
+    </div>
   </div>
 </template>
 
