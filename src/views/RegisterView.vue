5e0b65555b2d5fd53475afe2ab76bef0029f190d
Margit Toots <m.toots@hotmail.com>
2022-12-05 11:21:40 +0200

    registerview ja loginview inputboxid samasuguseks

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,29 +3,29 @@
 
   <div class="container mt-5">
     <div class="row justify-content-around">
-      <div class="col-4">
-        <div class="input-group mb-4">
-          <span class="input-group-text" id="inputGroup-sizing-default">Eesnimi*</span>
+      <div class="col-md-4">
+        <div class="input-group ms-3 mt-5 pt-5 mb-4">
+          <span class="input-group-text input-width" id="inputGroup-sizing-default">Eesnimi*</span>
           <input v-model="registerRequest.firstName" type="text" class="form-control" aria-label="Sizing example input"
                  aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-4">
-          <span class="input-group-text" id="inputGroup-sizing-default">Perekonnanimi*</span>
+        <div class="input-group ms-3 mb-4">
+          <span class="input-group-text input-width" id="inputGroup-sizing-default">Perekonnanimi*</span>
           <input v-model="registerRequest.lastName" type="text" class="form-control" aria-label="Sizing example input"
                  aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-4">
-          <span class="input-group-text" id="inputGroup-sizing-default">e-posti aadress*</span>
+        <div class="input-group ms-3 mb-4">
+          <span class="input-group-text input-width" id="inputGroup-sizing-default">e-posti aadress*</span>
           <input v-model="registerRequest.email" type="text" class="form-control" aria-label="Sizing example input"
                  aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-4">
-          <span class="input-group-text" id="inputGroup-sizing-default">Salasõna*</span>
+        <div class="input-group ms-3 mb-4">
+          <span class="input-group-text input-width" id="inputGroup-sizing-default">Salasõna*</span>
           <input v-model="registerRequest.password" type="password" class="form-control"
                  aria-label="Sizing example input"
                  aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-4">
+        <div class="input-group ms-3 mb-4">
           <span class="input-group-text" id="inputGroup-sizing-default">Korda salasõna*</span>
 
           <!--          todo:Korda salasõnale tuleb ka muutuja määrata, kontrollida eelmisega klappimist-->
@@ -33,18 +33,17 @@
           <input v-model="repeatPassword" type="password" class="form-control" aria-label="Sizing example input"
                  aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="row">*kohustuslikud väljad</div>
-        <div class="col-5">
-          <div class="row input-group mb-3">
+        <div class="row ms-2">*kohustuslikud väljad</div>
+        <div class="col-8 ps-5 ms-5">
+          <div class="row input-group mt-4">
             <button v-on:click="addCustomer()" type="button" class="btn btn-success">Loo konto</button>
           </div>
 
         </div>
       </div>
-      <div class="col-6">
-        <img
-            src="../assets/image_1.jpg"
-            alt="jalgratas" class="register-image">
+
+      <div class="col-sm-6 pt-5 pb-5 ms-1 mb-5">
+        <img src="../assets/image_1.jpg" alt="image_1" class="register-image">
       </div>
 
     </div>
