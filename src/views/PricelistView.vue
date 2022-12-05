1214b1c5a8d5d03179f0eff8c2f6e956602a7ed6
Margit Toots <m.toots@hotmail.com>
2022-12-05 23:12:55 +0200

    Pricelistview

--- src/views/PricelistView.vue
+++ src/views/PricelistView.vue
@@ -1,9 +1,111 @@
 <template>
-<div>
+  <div class="container p-3">
+    <h1 class="p-4">HINNAKIRI</h1>
+    <div class="row row-cols-3 g-4 m-lg-1">
+
+
+      <div class="col">
+        <div class="card h-100 text">
+          <div class="card-body">
+            <h5 class="mt-4 mb-3"><strong>KEVADINE RATTAHOOLDUS: 80€</strong></h5>
+            <div class="card-text ms-3 text-start justify-content-center">
+              <ul class="list-group ms-3 text-start">
+                <li>Kaelakausside puhastamine, määrimine, reguleerimine</li>
+                <li>Keskjooksu puhastamine, määrimine</li>
+                <li>Esi- ja tagarummu puhastamine, määrimine, reguleerimine</li>
+                <li>Pidurite reguleerimine ja kontroll</li>
+                <li>Esi- ja tagakäiguvahetaja reguleerimine</li>
+                <li>Kõikide trosside puhastamine</li>
+                <li>Jooksude/ratta rihtimine</li>
+                <li>Keti kulumise kontroll, puhastamine, õlitamine (vajadusel vahetus)</li>
+                <li>Rehvirõhu kontroll, kummide pumpamine</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col">
+        <div class="card border-light">
+          <img src="../assets/image_5.jpg" class="img-fluid" alt="image_5.jpg">
+        </div>
+        <div class="card-body">
+        </div>
+      </div>
+
+
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="mt-5 mb-3"><strong>RATTA PIDURITE HOOLDUS</strong></h5>
+            <ul class="card-text ms-3 text-start justify-content-center">
+              <li>Pidurilinkide vahetus 10.-</li>
+              <li>Piduri reguleerimine 6.-</li>
+              <li>Ketaspiduri reguleerimine 10.- kuni 20.-</li>
+              <li>Piduriklotside vahetus 5.-/pr</li>
+              <li>Piduritrossi vahetus 3.-</li>
+              <li>Ketaspiduri paigaldus või vahetus 10-15.-</li>
+              <li>Hüdrauliliste ketaspidurite hooldus, õli lisamine või vahetust 30.-</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card border-light">
+          <img src="../assets/image_6.jpg" class="img-fluid" alt="image_6.jpg">
+        </div>
+        <div class="card-body">
+        </div>
+      </div>
+
+
+      <div class="col">
+        <div class="card h-100 text">
+          <div class="card-body">
+            <h5 class="mt-4 mb-3"><strong>SÜGISENE RATTAHOOLDUS: 80€</strong></h5>
+            <div class="card-text ms-3 text-start justify-content-center">
+              <ul class="list-group ms-3 text-start">
+                <li>Kaelakausside puhastamine, määrimine, reguleerimine</li>
+                <li>Keskjooksu puhastamine, määrimine</li>
+                <li>Esi- ja tagarummu puhastamine, määrimine, reguleerimine</li>
+                <li>Pidurite reguleerimine ja kontroll</li>
+                <li>Esi- ja tagakäiguvahetaja reguleerimine</li>
+                <li>Kõikide trosside puhastamine</li>
+                <li>Jooksude/ratta rihtimine</li>
+                <li>Keti kulumise kontroll, puhastamine, õlitamine (vajadusel vahetus)</li>
+                <li>Rehvirõhu kontroll, kummide pumpamine</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
-</div>
+      <div class="col">
+        <div class="card h-100 text">
+          <div class="card-body">
+            <h5 class="mt-5 mb-3"><strong>PAKETT - KÄIGUD, KETT JA PIDURID</strong></h5>
+            <div class="card-text ms-3 text-start justify-content-center">
+              <ul class="list-group ms-3 text-start">
+                <li>Piduri reguleerimine</li>
+                <li>Piduritrossi ja -klotside vahetus</li>
+                <li>Pidurite reguleerimine ja kontroll</li>
+                <li>Esi- ja tagakäiguvahetaja reguleerimine</li>
+                <li>Kõikide trosside puhastamine</li>
+                <li>Jooksude/ratta rihtimine</li>
+                <li>Keti kulumise kontroll, puhastamine, õlitamine (vajadusel vahetus)</li>
+                <li>Rehvirõhu kontroll ja kummide pumpamine</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+  </div>
 
 
 </template>
